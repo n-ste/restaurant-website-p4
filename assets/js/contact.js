9dc610700799ec3ad6752d4282275a559c700bec
Nathan Steele <nathanisteele_1997@outlook.com>
2025-02-25 20:16:20 +0000

    Changes made to JavaScript file to ensure full messages are being sent

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -2,7 +2,7 @@ function sendMail(contactForm) {
     emailjs.send("gmail", "restaurant-contact", {
         "from_name": contactForm.name.value,
         "from_email": contactForm.emailaddress.value,
-        "project_request": contactForm.messagecontent.value
+        "submit_message": contactForm.messagecontent.value
     })
     .then(
         function(response) {
