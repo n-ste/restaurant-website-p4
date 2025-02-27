083b1bce9e7b8bf2d332bb805e25e467aa4274e0
Nathan Steele <nathanisteele_1997@outlook.com>
2025-02-27 13:59:38 +0000

    Refactored bookings page into base page and incorporated template inheritance

--- bookings/routes.py
+++ bookings/routes.py
@@ -5,4 +5,19 @@ from bookings.models import Reservation, Customer
 
 @app.route("/")
 def home():
-    return render_template("bookings.html")
+    return render_template("index.html")
+
+
+@app.route("/menu")
+def menu():
+    return render_template("menu.html")
+
+
+@app.route("/bookings")
+def bookings():
+    return render_template("reservations.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
